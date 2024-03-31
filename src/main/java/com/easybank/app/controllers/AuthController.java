411d689e65dc5a6dcf707a7a3c759f32ff59f0f1
lukas <lkab05@hotmail.com>
2024-03-31 12:26:31 -0300

    feat: add authentication controller layer (login and regiter endpoints)

---
+++ src/main/java/com/easybank/app/controllers/AuthController.java
@@ -0,0 +1,55 @@
+package com.easybank.app.controllers;
+
+import com.easybank.app.dto.LoginDTO;
+import com.easybank.app.dto.RegisterRequestDTO;
+import com.easybank.app.dto.ResponseDTO;
+import com.easybank.app.infra.security.TokenService;
+import com.easybank.app.model.User;
+import com.easybank.app.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginDTO dto) {
+        var user = userRepository.findByEmail(dto.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        if (passwordEncoder.matches(dto.password(), user.getPassword())) { // compare the password
+            var token = tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+        return ResponseEntity.badRequest().body("Invalid credentials");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO dto) {
+
+        Optional<User> user = userRepository.findByEmail(dto.email());
+        if (user.isEmpty()) {
+            var newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(dto.password()));
+            newUser.setEmail(dto.email());
+            newUser.setName(dto.name());
+            this.userRepository.save(newUser);
+            String token = tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        }
+
+        return ResponseEntity.badRequest().body("Invalid credentials");
+    }
+}
