d56aee3c949d0e04986e0c226a48638623093875
lucasbalves <lkab05@hotmail.com>
2024-05-22 18:49:57 -0300

    feat: add cors

---
+++ src/main/java/com/easybank/app/infra/security/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.easybank.app.infra.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3600); //
+    }
+}
